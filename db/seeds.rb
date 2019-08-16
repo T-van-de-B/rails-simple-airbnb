a568ed62855faee8678c5c2742917f46e2222a9e
Thomas Britz <britzthomas@gmail.com>
2019-08-16 10:52:37 +0100

    d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Garge',
+  address: 'Main',
+  description: 'Awesome',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Penthouse',
+  address: 'Parkroad',
+  description: 'Even better',
+  price_per_night: 200,
+  number_of_guests: 8
+)
